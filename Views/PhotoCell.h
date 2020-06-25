8bb0c3465d4e91ac98bf9852a7b918a3b3324173
alexoseg <aoseg@stanford.edu>
2020-06-25 09:55:30 -0700

    User can scroll through images loaded through Tumblr API

---
+++ Views/PhotoCell.h
@@ -0,0 +1,18 @@
+//
+//  PhotoCell.h
+//  Tumblr
+//
+//  Created by Alex Oseguera on 6/25/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *postPicView;
+
+@end
+
+NS_ASSUME_NONNULL_END
